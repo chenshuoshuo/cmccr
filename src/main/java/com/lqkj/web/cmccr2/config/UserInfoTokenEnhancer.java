c4171acc2b6ad10344334d88ddc41504b5bbf8ff
freewu <2217332562@qq.com>
2019-01-21 15:58:32 +0800

    修复token bug

--- src/main/java/com/lqkj/web/cmccr2/config/UserInfoTokenEnhancer.java
+++ src/main/java/com/lqkj/web/cmccr2/config/UserInfoTokenEnhancer.java
@@ -1,15 +1,13 @@
 package com.lqkj.web.cmccr2.config;
 
 import com.lqkj.web.cmccr2.modules.user.domain.CcrUser;
-import com.lqkj.web.cmccr2.modules.user.domain.CcrUserAuthority;
-import com.lqkj.web.cmccr2.modules.user.domain.CcrUserRule;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 用户信息
@@ -18,10 +16,16 @@ public class UserInfoTokenEnhancer implements TokenEnhancer {
 
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+        Object principal = authentication.getUserAuthentication().getPrincipal();
+
+        if (!(principal instanceof CcrUser)){
+            return accessToken;
+        }
+
         Set<String> scopes = new HashSet<>(20);
         scopes.addAll(accessToken.getScope());
 
-        CcrUser user = (CcrUser) authentication.getUserAuthentication().getPrincipal();
+        CcrUser user = (CcrUser) principal;
 
         user.getRules().forEach(v -> scopes.add("role_" + v.getContent()));
         user.getAuthorities().forEach(v -> scopes.add("authority_" + v.getContent()));
