ac197ef7c92657feca0c8a9357212bf12a6b4420
freewu <2217332562@qq.com>
2019-02-14 16:13:27 +0800

    升级版本 增加推送脚本

---
+++ upgrade.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+echo "开始打包jar"
+
+mvn clean package
+
+mv target/cmccr2-*.jar target/cmccr.jar
+
+echo "打包jar结束"
+
+echo "开始传输jar文件"
+
+rsync -avz --progress target/cmccr.jar lqkj@192.168.4.240:/home/lqkj/cmccr -e "ssh -p 1088"
+
+echo "传输jar文件结束"
+
+echo "开始重新服务"
+
+ssh -p 1088 root@192.168.4.240 "service cmccr restart;"
+
+echo "重启服务结束"
