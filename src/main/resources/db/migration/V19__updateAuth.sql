9ab030abfaf8ef437714d5e55c25f0b78c5356b5
freewu <2217332562@qq.com>
2019-03-26 17:04:45 +0800

    更新权限地址
    删除无效模块

---
+++ src/main/resources/db/migration/V19__updateAuth.sql
@@ -0,0 +1,7 @@
+delete
+from ccr_user_authority
+where authority_id in (26, 27, 31, 32, 33);
+
+update ccr_user_authority a set route = '/cmdbe' where a.authority_id = 28;
+update ccr_user_authority a set route = '/cmgis' where a.authority_id = 29;
+update ccr_user_authority a set route = '/cmips-manager' where a.authority_id = 30;
