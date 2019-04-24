c45be052058707516282808409e6ab00f622eebd
freewu <2217332562@qq.com>
2019-04-24 11:34:46 +0800

    修复错误的权限序列

--- src/main/resources/db/migration/V25__modelAuth.sql
+++ src/main/resources/db/migration/V25__modelAuth.sql
@@ -1,3 +1,5 @@
 INSERT INTO public.ccr_user_authority (authority_id, content, name, route, parent_id, type, icon, enabled, http_method,
                                        file_path)
 VALUES (87, 'model-manager', '专题图模块管理', '/model-manager', 31, 'm3820_menu', null, true, null, null);
+
+select setval('ccr_user_authority_authority_id_seq', 88);
