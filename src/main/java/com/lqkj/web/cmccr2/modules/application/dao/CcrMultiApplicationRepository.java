b826a414e2558261953949bba8835f7872df5d2c
freewu <2217332562@qq.com>
2019-05-05 11:14:49 +0800

    修复权限管理错误

--- src/main/java/com/lqkj/web/cmccr2/modules/application/dao/CcrMultiApplicationRepository.java
+++ src/main/java/com/lqkj/web/cmccr2/modules/application/dao/CcrMultiApplicationRepository.java
@@ -2,8 +2,13 @@ package com.lqkj.web.cmccr2.modules.application.dao;
 
 import com.lqkj.web.cmccr2.modules.application.domain.CcrMultiApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CcrMultiApplicationRepository extends JpaRepository<CcrMultiApplication,Long> {
+
+    @Query("select app from CcrMultiApplication app where app.webURL=:webURL")
+    CcrMultiApplication findByWebURL(@Param("webURL") String webURL);
 }
