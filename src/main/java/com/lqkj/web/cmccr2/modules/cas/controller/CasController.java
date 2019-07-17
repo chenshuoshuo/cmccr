0776ecd80c739d59f68b82928531513e20ca8c71
ztc <ztc123>
2019-07-17 10:33:10 +0800

    Https的请求转为Http

--- src/main/java/com/lqkj/web/cmccr2/modules/cas/controller/CasController.java
+++ src/main/java/com/lqkj/web/cmccr2/modules/cas/controller/CasController.java
@@ -1,12 +1,18 @@
 package com.lqkj.web.cmccr2.modules.cas.controller;
 
 import com.lqkj.web.cmccr2.APIVersion;
+import com.lqkj.web.cmccr2.utils.HttpUtils;
+import io.micrometer.core.instrument.util.JsonUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import okhttp3.Response;
+import org.apache.http.HttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 /**
  * @ClassName CasLoginController
@@ -21,8 +27,16 @@ public class CasController {
 
     @ApiOperation("cas单点登录获取用户信息")
     @GetMapping("/center/cas/" + APIVersion.V1 + "/getUser/")
-    public String casToUser(@ApiParam(name = "carLoginUrl",value = "获取用户信息访问地址",required = true)@RequestParam(required = true)String carLoginUrl){
-        return "asdfa";
+    public String casToUser(@ApiParam(name = "casLoginUrl",value = "获取用户信息访问地址",required = true)@RequestParam(required = true)String casLoginUrl){
+        try {
+            HttpUtils httpUtils=new HttpUtils();
+            Response response=httpUtils.getRequest(casLoginUrl,20,null);
+            return response.body().string();
+        }catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
 }
