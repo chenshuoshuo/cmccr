2c27e54abdebe93fc482611e3a6ef0940e8ea095
gjx <gjxchn@126.com>
2019-08-28 21:32:10 +0800

    中控权限管理增加父级权限

---
+++ src/main/resources/db/migration/V35__update_cmccr_auth.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.ccr_user_authority VALUES (129, 'CMCCR', '中控发布系统', '/cmccr', NULL, 'menu', NULL, true, NULL);
+
+
+update ccr_user_authority set parent_id = 129 where authority_id = 14;
+update ccr_user_authority set parent_id = 129 where authority_id = 1;
+update ccr_user_authority set parent_id = 129 where authority_id = 3;
+update ccr_user_authority set parent_id = 129 where authority_id = 5;
+update ccr_user_authority set parent_id = 129 where authority_id = 8;
+update ccr_user_authority set parent_id = 129 where authority_id = 10;
+update ccr_user_authority set parent_id = 129 where authority_id = 16;
+update ccr_user_authority set parent_id = 129 where authority_id = 24;
+
+INSERT INTO public.ccr_rule_to_authority(1,129);
