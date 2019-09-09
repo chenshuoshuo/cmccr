9643fa5ab201eef17b9f3bbec2106bc338f5132b
chenshuo <382057753@qq.com>
2019-09-09 11:00:03 +0800

    1、更改热力图和迁徙图父级ID 2、更改迁徙图 热力图 迁徙图区域默认不可见

---
+++ src/main/resources/db/migration/V39__update_user_authority.sql
@@ -0,0 +1,5 @@
+update ccr_user_authority set parent_id = 8,enabled = false where authority_id = 94;
+
+update ccr_user_authority set parent_id = 8,enabled = false where authority_id = 93;
+
+update ccr_user_authority set enabled = false where authority_id = 95;
